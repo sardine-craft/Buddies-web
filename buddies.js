c0dcd1913c7ed6441fd3c61e23cca8e673ec3243
Ian Cummings <misterian.cummings@gmail.com>
2020-09-04 15:59:44 -0400

    removed countdown, float ids, cleaned up html.

--- buddies.js
+++ buddies.js
@@ -1,33 +1,4 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Aug 31, 2020 12:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get todays date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in an element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("cs1").style.display = "none";
-    document.getElementById("countdown").style.display = "none";
-    alert("Stage One released.");
-  }
-}, 1000);
 
 function Home() {
     document.getElementById("title").innerHTML = "Home.";
@@ -49,20 +20,4 @@ function hide() {
 }
 
 
-function startup() {
-    var str = window.location.href;
-    alert("The URL of this page is: " + str);
-    if (str.includes("home")) {
-    alert("You have been granted acces, Ian.");
-    document.getElementById("cs1").style.display = "none";
-    document.getElementById("countdown").style.display = "none";
-
-    }  
-}
-
-function fool() {
-    alert("Bro, come on. Where did you think that would take you?");
-}
-
-window.onload = startup();
 
